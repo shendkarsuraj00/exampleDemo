286caa061721606c0175b14cdded764f97b85fd5
Pratik Pol <pratikpol@pratiks-mac-mini.local>
2017-12-19 16:52:58 +0530

    Changes in view controller

--- samoleDemo/ViewController.swift
+++ samoleDemo/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        print("Hello")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
